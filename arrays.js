df07913b1863e7c1f059111bea852ea33ec07040
Lien Phan <whyofcourse@gmail.com>
2017-09-10 17:50:33 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,31 +14,31 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 //   var newArray = [..array, element]
 //   return newArray
 // }
-// 
+//
 // function destructivelyAddElementToEndOfArray(array, element){
 //   array.push(element)
 //   return array
 // }
-// 
+//
 // function accessElementInArray(array, index){
 //   return array[index]
 // }
-// 
+//
 // function destructivelyRemoveElementFromBeginningOfArray(array){
 //   array.shift()
 //   return array
 // }
-// 
+//
 // function removeElementFromEndOfArray(array){
 //   var newArray = array.slice(1)
 //   return newArray
 // }
-// 
+//
 // function destructivelyRemoveElementFromEndOfArray(array){
 //   array.pop()
 //   return array
 // }
-// 
+//
 // function removeElementFromEndOfArray(array){
 //   var newArray = array.slice(0, array.length -1)
 //   return newArray
